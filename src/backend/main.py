43692bd1fafc5d12ac5017d390314ec0a99000ef
LewisDean22 <lewisdean22@outlook.com>
2026-02-23 22:50:15 +0000

    <feat>(upload): Began implementing the track upload to client storage

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import tracks, health
 from contextlib import asynccontextmanager
 
@@ -9,8 +10,15 @@ async def lifespan(app: FastAPI):
     yield  # app runs here
     # shutdown
 
-# CORS Middleware?
+app = FastAPI(lifespan=lifespan, root_path="/api")  # matches the vite proxy
 
-app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(tracks.router)
 app.include_router(health.router)
--- src/frontend/src/components/MainPanel.jsx
+++ src/frontend/src/components/MainPanel.jsx
@@ -1,9 +1,36 @@
+import { useRef, useState } from 'react'
+
+
 export default function MainPanel() {
+  const fileInputRef = useRef(null)
+  const [files, setFiles] = useState([])
+
+
+  const handleFileUpload = (e) => {
+  const selectedFiles = Array.from(e.target.files)
+  setFiles(selectedFiles)  // stores File objects in React state
+  }
+
   return (
-    <main className="main">
-      <button id="upload-button">
-        Click here to upload tracks
+  <main className="main">
+    <input
+      type="file"
+      multiple
+      accept="audio/*"
+      onChange={handleFileUpload}
+      style={{ display: 'none' }} // hidden so just a button
+      ref={fileInputRef}
+    />
+    {files.length === 0 ? (
+      <button id="upload-button" onClick={() => fileInputRef.current.click()}>
+        Upload Tracks
       </button>
-    </main>
+    ) : (
+      <div className="split-pane">
+        TRACKLIST GOES HERE...
+        {/* Tracklist left, analysis controls right */}
+      </div>
+    )}
+  </main>
   )
 }
